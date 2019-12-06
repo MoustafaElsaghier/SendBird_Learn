c84136f4ee8d56557a339378cc8137f04975ccf6
EL_Saghier <thedreamereng@gmail.com>
2019-12-06 14:42:31 +0200

    create model for messages type

---
+++ app/src/main/java/elsaghier/com/sendbird_learn/Models/ChatRoomModel.java
@@ -0,0 +1,42 @@
+package elsaghier.com.sendbird_learn.Models;
+
+public class ChatRoomModel {
+
+    private String imageUrl;
+    private String chatRoomName;
+    private String lastMessage;
+    private String room_id;
+
+    public String getRoom_id() {
+        return room_id;
+    }
+
+    public void setRoom_id(String room_id) {
+        this.room_id = room_id;
+    }
+
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getChatRoomName() {
+        return chatRoomName;
+    }
+
+    public void setChatRoomName(String chatRoomName) {
+        this.chatRoomName = chatRoomName;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+}
