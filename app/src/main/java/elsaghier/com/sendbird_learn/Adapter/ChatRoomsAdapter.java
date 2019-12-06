e9d53bc9e8a3bd1e7732f6bdf017a645bf97d28d
EL_Saghier <thedreamereng@gmail.com>
2019-12-06 15:34:02 +0200

    build adapter for chat room layout

---
+++ app/src/main/java/elsaghier/com/sendbird_learn/Adapter/ChatRoomsAdapter.java
@@ -0,0 +1,90 @@
+package elsaghier.com.sendbird_learn.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import elsaghier.com.sendbird_learn.ChatActivity;
+import elsaghier.com.sendbird_learn.Models.ChatRoomModel;
+import elsaghier.com.sendbird_learn.R;
+
+public class ChatRoomsAdapter extends
+        RecyclerView.Adapter<ChatRoomsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<ChatRoomModel> list;
+
+    public ChatRoomsAdapter(Context context, List<ChatRoomModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.chat_item_layout, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ChatRoomModel item = list.get(position);
+
+        Glide.with(context).load(item.getImageUrl()).into(holder.imageView);
+
+        holder.chatRoomName.setText(item.getChatRoomName());
+        holder.lastMessage.setText(item.getLastMessage());
+        //Todo: Setup viewholder for item 
+        holder.bind(item);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return list == null ? 0 : list.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        // Todo Butterknife bindings
+
+        CircleImageView imageView;
+        TextView chatRoomName, lastMessage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.chat_room_image);
+            chatRoomName = itemView.findViewById(R.id.chat_room_name);
+            lastMessage = itemView.findViewById(R.id.last_message);
+        }
+
+        void bind(final ChatRoomModel model) {
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, ChatActivity.class);
+                    intent.putExtra("room_name", model.getChatRoomName());
+                    intent.putExtra("room_id", model.getRoom_id());
+                    intent.putExtra("room_image", model.getImageUrl());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
+}
